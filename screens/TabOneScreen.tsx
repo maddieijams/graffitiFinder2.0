d87258c1bef88ba6571a96b56c9e432350a9262c
Maddie Ijams <maddieijams@gmail.com>
2020-09-29 19:17:03 -0400

    image upload and working through how to store image in db properly

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,77 +1,160 @@
-import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Image, Platform } from "react-native";
+import { useForm } from "react-hook-form";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
+import { ImagePickerResult } from "expo-image-picker";
+import { ImageInfo } from "expo-image-picker/build/ImagePicker.types";
 
-export interface CreateUpdateForm {
+export interface FormData {
   title: string;
-  imageUrl: string;
-  knowBefore: string;
+  image: string;
+  info: string;
   // * might be numbers
-  lat: string;
-  long: string;
+  lat: number;
+  lng: number;
 }
 
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 export default function TabOneScreen() {
-  const [createForm, setCreateForm] = useState<CreateUpdateForm>({
-    title: "",
-    imageUrl: "",
-    knowBefore: "",
-    lat: "",
-    long: "",
-  });
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  //find type
+  const [photo, setPhoto] = useState<any>();
+
+  const getPermissionAsync = async () => {
+    if (Platform.OS !== "web") {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
+  // const handleUploadPhoto = () => {
+  //   fetch("http://localhost:3000/api/upload", {
+  //     method: "POST",
+  //     body: photo && createFormData(photo, { userId: "123" }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       console.log("upload succes", response);
+  //       alert("Upload success!");
+  //       setPhoto(undefined);
+  //     })
+  //     .catch((error) => {
+  //       console.log("upload error", error);
+  //       alert("Upload failed!");
+  //     });
+  // };
+
+  const onSubmit = (data: FormData) => {
+    fetch(`http://localhost:8000/mobile/create`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res: Response) => res.json())
+      .then((data: any) => {
+        console.log(data);
+        // this.props.fetchGraffiti();
+        // this.setState({
+        //   title: '',
+        //   image:'',
+        //   info: '',
+        //   lat: '',
+        //   lng: ''
+        // })
+      });
+  };
+
+  const createFormData = (result: ImageInfo) => {
+    const data = new FormData();
+    console.log("create form data");
+    data.append("uri", result.uri);
+    return data;
+  };
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        setPhoto(result.uri);
+        console.log(result);
+        fetch("http://localhost:8000/mobile/upload", {
+          method: "POST",
+          body: result && createFormData(result),
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            console.log("upload succes", response);
+            alert("Upload success!");
+            setPhoto(undefined);
+          })
+          .catch((error) => {
+            console.log("upload error", error);
+            alert("Upload failed!");
+          });
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View
         style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path='/screens/TabOneScreen.tsx' />
-
-      <Text>Title</Text>
-      <TextInput
-        value={createForm.title}
-        placeholder='Type here...'
-        onChangeText={(text) => {
-          setCreateForm({ ...createForm, title: text });
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {photo && (
+        <Image source={{ uri: photo }} style={{ width: 200, height: 200 }} />
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Title</label>
+        <input name="title" ref={register} />
+        <label>Image URL</label>
+        <input name="image" ref={register} />
+        <label>Know Before You Go</label>
+        <input name="info" ref={register} />
+        <label>Latitude</label>
+        <input name="lat" ref={register} />
+        <label>Longitude</label>
+        <input name="lng" ref={register} />
+        {/* <button
+        type="button"
+        onClick={() => {
+          setValue("lastName", "luo"); // ✅
+          setValue("firstName", true); // ❌: true is not string
+          errors.bill; // ❌: property bill does not exist
         }}
-      />
-      <Text>Image</Text>
-      <TextInput
-        value={createForm.imageUrl}
-        placeholder='Type here...'
-        onChangeText={(text) => {
-          setCreateForm({ ...createForm, imageUrl: text });
-        }}
-      />
-      <Text>Know Before You Go</Text>
-      <TextInput
-        value={createForm.knowBefore}
-        placeholder='Type here...'
-        onChangeText={(text) => {
-          setCreateForm({ ...createForm, knowBefore: text });
-        }}
-      />
-      <Text>Latitude</Text>
-      <TextInput
-        value={createForm.lat}
-        placeholder='Type here...'
-        onChangeText={(text) => {
-          setCreateForm({ ...createForm, lat: text });
-        }}
-      />
-      <Text>Longitude</Text>
-      <TextInput
-        value={createForm.long}
-        placeholder='Type here...'
-        onChangeText={(text) => {
-          setCreateForm({ ...createForm, long: text });
-        }}
-      />
+      >
+        SetValue
+      </button> */}
+        <input type="submit" />
+      </form>
     </View>
   );
 }
